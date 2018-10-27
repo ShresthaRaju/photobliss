5eb978bc2c1db19d4b76d4002c20614579880ac3
ShresthaRaju <rajshres07@gmail.com>
2018-10-27 18:44:53 +0545

    Added SignUp/SignIn form

--- resources/views/auth/sign_in.blade.php
+++ resources/views/auth/sign_in.blade.php
@@ -3,7 +3,6 @@
 @section('body_class','login-page sidebar-collapse')
 @section('main_content')
 <div class="page-header header-filter" style="background-image: url({{ asset('images/sign_in_form_bg.jpeg') }}); background-size:cover; background-position:center;">
-	<sign-in>
-	</sign-in>
+	<sign-in></sign-in>
 </div>
 @endsection
